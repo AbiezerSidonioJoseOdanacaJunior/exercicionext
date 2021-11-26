08ee70e038530d3d014f94c269a6d2ed98e95095
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-26 13:41:37 -0300

    Crriando instancias a passar parametro

--- exercicionext/pages/basicos/usandoTitulo.jsx
+++ exercicionext/pages/basicos/usandoTitulo.jsx
@@ -2,7 +2,20 @@ import Titulo from "../../components/Titulo"
 export default function usandoTitulo() {
     return (
         <div>
-            <Titulo/>
+            <Titulo
+            principal = "Págna Principal"
+            secundario = "Págna de Edtar, cadastrar, excluir..."
+            />
+            <Titulo
+            principal = "Faz seu Login"
+            secundario = "Digite seu email por gentileza!"
+            pequeno = {true}
+            />
+            <Titulo
+            principal = "Faz seu Login"
+            secundario = "Digite seu email por gentileza!"
+            pequeno 
+            />
         </div>
     )
 }
