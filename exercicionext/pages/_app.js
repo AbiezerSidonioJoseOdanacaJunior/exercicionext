42b169a93ed61718f12481c88b1958e0d3adae12
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-26 22:54:31 -0300

    Integração com css

--- exercicionext/pages/_app.js
+++ exercicionext/pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css'
+import '../pages/basicos/css/integracao.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
---
+++ exercicionext/pages/basicos/css/integracao.jsx
@@ -0,0 +1,12 @@
+//Não posso importatr o css aqui diretamente com import
+//mas sim dentro do app.js
+
+export default function integracao() {
+    return (
+        <>
+            <div className="vermelha">Item#01</div>
+            <div className="azul">Item#02</div>
+            <div className="branca">Item#03</div>
+        </>
+    )
+}
