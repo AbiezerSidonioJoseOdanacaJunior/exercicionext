aac9b7445263580d4a0caa7b77efcd7f02105cf1
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-11-28 11:25:22 -0300

    Aplicando css

--- exercicionext/components/SomaUm.jsx
+++ exercicionext/components/SomaUm.jsx
@@ -1,9 +1,8 @@
 export default function SomaUm(props) {
-    //props.numero++
+    // props.numero++
     return (
         <div>
             <h1>{props.numero + 1}</h1>
         </div>
     )
-    
 }
---
+++ exercicionext/pages/css/usandoEstilo.jsx
@@ -0,0 +1,10 @@
+import Estilo from "../../components/Estilo";
+
+export default function usandoEstilo() {
+    return ( 
+       <div>
+           <Estilo numero = {30} />
+           <Estilo numero = {-50} />
+       </div>
+    )
+}
