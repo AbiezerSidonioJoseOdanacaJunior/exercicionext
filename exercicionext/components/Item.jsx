c42ddf80bcf81643e9a143ea06e59a29887de419
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-02 10:51:59 -0300

    Props children

---
+++ exercicionext/components/Item.jsx
@@ -0,0 +1,5 @@
+export default function (props) {
+    return (
+        <li>{props.conteudo}</li>
+    )
+}
---
+++ exercicionext/pages/basicos/componentesComFilhos.jsx
@@ -0,0 +1,15 @@
+import Item from "../../components/Item";
+import Lista from "../../components/Lista";
+
+export default function componentesComFilhos () {
+    return (
+        <Lista>
+            <Item conteudo="#Item1" />
+            <Item conteudo="#Item2" />
+            <Item conteudo="#Item3" />
+            <Item conteudo="#Item4" />
+            <Item conteudo="#Item5" />
+            <Item conteudo="#Item6" />
+        </Lista>
+    )
+}
