829aa702ea654a0eb46eaf40210675e6aa5d6024
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-02 16:25:46 -0300

    Comunicacao Direta

---
+++ exercicionext/components/direta/Pai.jsx
@@ -0,0 +1,14 @@
+import Filho from "./Filho";
+
+export default function Pai() {
+    return (
+        <div>
+            <Filho familia="José" />
+            <Filho familia="Hoseias" />
+            <Filho familia="Efraim" />
+            <Filho familia="Gelson" />
+            <Filho familia="Abiezer" />
+            <Filho familia="Azarias" />
+        </div>
+    )
+}
