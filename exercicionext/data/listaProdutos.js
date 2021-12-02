5f8fb1a7b7b3b060d71ad51ca4c7f8ca40b506b5
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-01 22:55:43 -0300

    Repeticao 2

---
+++ exercicionext/data/listaProdutos.js
@@ -0,0 +1,11 @@
+import Produto from "../model/produto";
+
+export default [
+    new Produto(1, "iPad", 7500),
+    new Produto(2, "Notebook", 5500),
+    new Produto(3, "Computador", 4500),
+    new Produto(4, "Iphone", 3500),
+    new Produto(5, "Sansung", 2500),
+    new Produto(6, "MacBook Air", 15000),
+]
+
