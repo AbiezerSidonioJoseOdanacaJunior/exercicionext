1b462230852a5f9fb78bdf6b61f0ad73c52d5bab
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-02 10:15:43 -0300

    Condicional1

---
+++ exercicionext/pages/render/condicional1.jsx
@@ -0,0 +1,14 @@
+import SomntePar from "../../components/SomentePar";
+
+export default function Ccondicional1() {
+    
+    return (
+        <div>
+            <SomntePar numero = {2} />
+            <SomntePar numero = {4} />
+            <SomntePar numero = {5} />
+            <SomntePar numero = {16} />
+            <SomntePar numero = {9} /> 
+        </div>
+    )
+}
