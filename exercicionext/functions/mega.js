bb54d8e0b8b923df9b0909d2b966dab7acce62e7
AbiezerSidonioJoseOdanacaJunior <abiezerodanacajosejunior@gmail.com>
2021-12-06 13:58:01 -0300

    Logica para gerar valores aleatores

---
+++ exercicionext/functions/mega.js
@@ -0,0 +1,18 @@
+export default function mega(qtd = 6, numeros = [] ) {
+  if(qtd < 6 && qtd > 60) {
+      throw "QUANTIDADE INVÁLIDA!"
+  }
+
+
+  if(numeros.length === qtd) {
+      return numeros.sort((n1, n2) => n1 - n2)
+  }
+
+  const numeroAleatorio = parseInt(Math.random() * 60 ) +1
+  if(!numeros.includes(numeroAleatorio)) {
+      return mega(qtd, [...numeros, numeroAleatorio])
+  }else{
+      return mega(qtd, numeros)
+  }
+
+}
